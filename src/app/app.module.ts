e5330635948f6ebec61656c456a2c84239480f67
annngure <annwanjikungure@gmail.com>
2022-01-16 17:38:18 +0300

    add components and class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormComponent } from './form/form.component';
+import { UserComponent } from './user/user.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    UserComponent,
+    RepositoriesComponent
   ],
   imports: [
     BrowserModule,
